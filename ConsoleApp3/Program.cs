8fb2f0d142dc503d280693fb114fdd9cfb1c86fa
JelenaRunje01 <101826968+JelenaRunje01@users.noreply.github.com>
2022-03-17 19:33:41 +0100

    Update Program.cs
    dodan komentar

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,7 +4,7 @@ namespace ConsoleApp3
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//komentar
         {
             Console.WriteLine("Hello World!");
         }
